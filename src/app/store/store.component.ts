3df0dff369166605f4be16687b01d4408e6e3c61
skorpion <savinov_vu@inbox.ru>
2018-01-21 12:08:06 +0800

    simple pagenaiting.

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -9,8 +9,9 @@ import {Product} from '../model/product.model';
   moduleId: module.id
 })
 export class StoreComponent implements OnInit {
-
   public selectedCategory = null;
+  public productsPerPage = 4;
+  public selectedPage = 1;
 
   constructor(private repository: ProductRepository) {
   }
@@ -19,15 +20,29 @@ export class StoreComponent implements OnInit {
   }
 
   get products(): Product[] {
-    return this.repository.getProducts(this.selectedCategory);
+    let pageIndex = (this.selectedPage - 1) * this.productsPerPage;
+    return this.repository.getProducts(this.selectedCategory).slice(pageIndex, pageIndex + this.productsPerPage);
   }
 
   get categories(): string[] {
     return this.repository.getCategories();
   }
 
-  changeCategory(newCategory?: string){
+  changeCategory(newCategory?: string) {
     this.selectedCategory = newCategory;
+  }
+
+  changePage(newPage: number) {
+    this.selectedPage =  Number(newPage);
+  }
+
+  changePageSize(newSize: number) {
+    this.productsPerPage = newSize;
+    this.changePage(1);
+  }
+  get pageNumbers(): number[]{
+    return Array(Math.ceil(this.repository.getProducts(this.selectedCategory).length / this.productsPerPage))
+      .fill(0).map((x, i) => i + 1);
   }
 
 
